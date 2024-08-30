d92656369e5930245b34f3dad70878b9e7fa8ca9
Linh <121621222+TuanLinh1892004@users.noreply.github.com>
2024-08-30 10:49:48 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,7 @@ require("dotenv").config()
 
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
 const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const BSC_PRIVATE_KEY = process.env.BSC_PRIVATE_KEY;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.24",
