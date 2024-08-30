c846d3d388ece79342fa4a44bddcf20aaab88544
Linh <121621222+TuanLinh1892004@users.noreply.github.com>
2024-08-30 12:30:33 +0700

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,19 +3,20 @@ require("dotenv").config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 
-const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY;
-const INFURA_API_KEY = process.env.INFURA_API_KEY;
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
+const ETHERSCAN_API_KEY   = process.env.ETHERSCAN_API_KEY;
+const INFURA_API_KEY      = process.env.INFURA_API_KEY;
+const PRIVATE_KEY         = process.env.PRIVATE_KEY;
+const MNEMONIC            = process.env.MNEMONIC;
+const BSCSCAN_API_KEY     = process.env.BSCSCAN_API_KEY;
 
 module.exports = {
   solidity: "0.8.24",
-  etherscan: {
-    apiKey: ETHERSCAN_API_KEY,
-  },
   networks: {
-    bsc: {
-      url: `https://bsc-testnet.infura.io/v3/${INFURA_API_KEY}`,
-      accounts: [PRIVATE_KEY],
+    bscTestnet: {
+      url: "https://bsc-testnet-rpc.publicnode.com",
+      gasPrice: 20000000000,
+      chainId: 97,
+      accounts: {mnemonic: MNEMONIC},
     },
     sepolia: {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
@@ -24,8 +25,8 @@ module.exports = {
   },
   etherscan: {
     apiKey: {
-      bsc: ETHERSCAN_API_KEY,
       sepolia: ETHERSCAN_API_KEY,
+      bscTestnet: BSCSCAN_API_KEY,
     },
   },
 };
